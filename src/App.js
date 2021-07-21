94c940bb61a4fc18dfcb4380e80a157b24797909
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-07-21 15:01:02 -0700

    Copied css and showing a container with h1 title displayed. Created UserForm.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <header><h1>User Onboarding</h1></header>
+    </div>
+  );
+}
+
+export default App;
