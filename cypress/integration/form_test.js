05179d1003d52a33a9c05e6f03321cd3038d23fe
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-07-22 16:24:28 -0700

    Added test to verify name is being set and verifying against correct value

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,16 +1,18 @@
 //writing tests here
 describe('User-Onboarding-App', () =>{
+    //visit localhost project website
     beforeEach(() =>{
         cy.visit('http://localhost:3000')
     })
+
     //ensuring Cypress is setup
     it('sanity check for making sure everything is setup',()=>{
         //check some assertions here
         expect(1+2).to.equal(3)     //strict
         expect(2+2).not.to.equal(5)
     })
-    //ensuring we can read elements
 
+    //ensuring we can read elements
     it('check that elements exist', () =>{
         foobarInput().should('not.exist')
         submitBtn().should('exist')
@@ -19,8 +21,17 @@ describe('User-Onboarding-App', () =>{
         passwordInput().should('exist')
         termsInput().should('exist')
     })
+
+    //inputting a user test
+    it('test inputting into the form and checking expected value', () =>{
+        nameInput().type("Johnathan")
+        nameInput().should('have.value', 'Johnathan')
+    })
+
+
 })
 
+//definitions of components
 const foobarInput = () => cy.get('input[name=foobar]')
 const submitBtn = () => cy.get('button[id="submitBtn"]')
 const nameInput = () => cy.get('input[name="name"]')
